f986fee58348eafc493488040b0e4ab950be028c
Julian Baumann <git@julba.de>
2023-12-22 14:54:46 +0100

    Refactor

--- Samples/SdkSample/Program.cs
+++ Samples/SdkSample/Program.cs
@@ -16,32 +16,32 @@ var servoAngles = await connection.Get<CurrentServoAngles>();
 Console.WriteLine($"Angles: Bottom {servoAngles.BottomServo}, Left: {servoAngles.LeftServo} Right: {servoAngles.RightServo} Height: {servoAngles.HeightServo}");
 
 // Resetting position
-await connection.QueryCommand(new MovePolar(Stretch: 100, Rotation: 0, Height: 80, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 100, Rotation: 0, Height: 80, Speed: 10));
 
 Thread.Sleep(3000);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 80, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 80, Speed: 10));
 
 Thread.Sleep(1000);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
 
 // Wink
 Thread.Sleep(1000);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 250, Speed: 10));
 
 Thread.Sleep(200);
-await connection.QueryCommand(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
+await connection.Execute(new MovePolar(Stretch: 200, Rotation: 100, Height: 200, Speed: 10));
